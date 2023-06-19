253ee4c16bf8a597fe731d2f5e355f9abdf322c1
Bhuwan <bhuwantiwari2002@gmail.com>
2023-06-18 20:16:28 -0500

    Added the navbar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 /* Entry point of the project */
 import '../styles/globals.css';
 import { Inter } from 'next/font/google'
+import Navbar from '../components/navbar'; // adjust the path as necessary
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,9 +13,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-
-      <body className={inter.className}>{children}</body>
-    </html>
+    <div>
+      <Navbar />
+      <div className={inter.className}>{children}</div>
+    </div>
   )
 }
---
+++ components/navbar.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import '../styles/navbar.css';
+
+function navbar() {
+  return (
+    <nav role="navigation">
+        <div id="menuToggle">
+        <input type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <ul id="menu">
+            <li><Link href="/hotlines">Hotlines</Link></li>
+            <li><Link href="/resources">Resources</Link></li>
+        </ul>
+        </div>
+    </nav>
+    
+  );
+}
+
+export default navbar;
+
+
