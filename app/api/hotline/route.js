0340b78da274fd0f7eff288cb0bdc2517d1e1f2a
Bhuwan <bhuwantiwari2002@gmail.com>
2023-07-08 13:17:37 -0500

    There should be no errors in the app

--- app/api/hotline/route.js
+++ app/api/hotline/route.js
@@ -1,4 +1,4 @@
 export async function GET(request) {
-    const data = require('../data/hotlines.json');
+    const data = require('../Data/hotlines.json');
     return new Response(JSON.stringify(data));
 }
