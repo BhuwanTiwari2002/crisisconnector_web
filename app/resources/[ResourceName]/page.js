193c27047f0fff9dae67a99856a3ce3f2aee4007
Bhuwan <bhuwantiwari2002@gmail.com>
2023-07-09 11:40:37 -0500

    Added some changes to .json and resource

--- app/resources/[ResourceName]/page.js
+++ app/resources/[ResourceName]/page.js
@@ -12,38 +12,41 @@ const getHotlineData = async (url) => {
 const ResourceName = async ({ params }) => {
   let data = await getHotlineData('resource');
   return (
-    <body>
-            <h1>Resource</h1>
-            {
-              data.map(({category, types}) => {
-                  return (
-                    <div id='detailsContainer'>
-                      <div>
-                            {types.map(({title, description, details}) => {
-                              return (
-                                <div className='resource-item'>
-                                  {(() => {
-                                    if (params.ResourceName == title) {
-                                      return(
-                                        <>
-                                          <h1>{title}</h1>
-                                          <p>{description}</p>
-                                          <p>{details}</p>
-                                        </>
-                                      );
-                                      
-                                    } 
-                                  })()}
-                                </div>
-                              );
-                            })}
-                      </div>
-                    </div>
-                  );
-              })
-            }
-      </body>
-
+      <body>
+      <h1>Resource</h1>
+      {
+        data.map(({category, types}) => {
+          return (
+            <div className='detailsContainer'>
+              <h2>{category}</h2>
+              {types.map(({title, description, details, image_path}) => {
+                return (
+                  <div className='resource-item'>
+                    {(() => {
+                      if (params.ResourceName == title) {
+                        return (
+                          <>
+                            <h1>{title}</h1>
+                            <img src={image_path} alt={title} />
+                            <p>{description}</p>
+                            <h2>Introduction</h2>
+                            <p>{details.introduction}</p>
+                            <h2>Symptoms</h2>
+                            <ul>{details.symptoms.map(symptom => <li>{symptom}</li>)}</ul>
+                            <h2>Causes</h2>
+                            <ul>{details.causes.map(cause => <li>{cause}</li>)}</ul>
+                          </>
+                        );
+                      } 
+                    })()}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })
+      }
+    </body>
   )
 }
 
