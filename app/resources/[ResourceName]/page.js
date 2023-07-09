5e7ea3abd7ac122b73f20078a9268bff3c826378
Bhuwan <bhuwantiwari2002@gmail.com>
2023-07-09 07:47:40 -0500

    Updated data and started on Resource Name

--- app/resources/[ResourceName]/page.js
+++ app/resources/[ResourceName]/page.js
@@ -1,8 +1,45 @@
 import React from 'react'
+const getHotlineData = async (url) => {
+  try {
+      const response = await fetch('http://localhost:3000/api/' + url);
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error(error);
+  }
+}
 
-const ResourceName = () => {
+const ResourceName = async ({ params }) => {
+  let data = await getHotlineData('resource');
+  // if (params.ResourceName == ) {
+  //   element = <p>The condition is true!</p>;
+  // } else {
+  //   element = <p>The condition is false!</p>;
+  // }
   return (
-    <div>ResourceName</div>
+    <body className='container' id="ResourcesMainContainer">
+            <h1>Resource</h1>
+            {
+              data.map(({category, types}) => {
+                  return (
+                    <div>
+                      <div>
+                      </div>
+                      <div className='resource-grid'>
+                            {types.map(({title, description, image_path}) => {
+                              return (
+                                <div className='resource-item'>
+                                  <h1>Test</h1>
+                                </div>
+                              );
+                            })}
+                      </div>
+                    </div>
+                  );
+              })
+            }
+          </body>
+
   )
 }
 
