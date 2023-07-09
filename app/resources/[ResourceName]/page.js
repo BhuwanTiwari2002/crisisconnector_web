c3b3a4d300db2ecc4ebd9e7e1de3331bac400209
Bhuwan <bhuwantiwari2002@gmail.com>
2023-07-09 13:33:10 -0500

    Some changes to ResourceName

--- app/resources/[ResourceName]/page.js
+++ app/resources/[ResourceName]/page.js
@@ -40,7 +40,7 @@ const ResourceName = async ({ params }) => {
                     })()}
                   </div>
                 );
-              })}
+              })}              
             </div>
           );
         })
